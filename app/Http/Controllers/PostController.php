8ec5a7726cb4b058e2f1bdc8e4ec13069cfb274b
yukirinmaru <ynishida1225@gmail.com>
2024-03-20 19:02:40 +0900

    8-2コミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,8 +8,19 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+ {
+     return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);  
+       //getPaginateByLimit()はPost.phpで定義したメソッドです。
+ }
+/**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+public function show(Post $post)
 {
-    return $post->get();//$postの中身を戻り値にする。
+    return view('posts.show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
 }
-    //
 }
